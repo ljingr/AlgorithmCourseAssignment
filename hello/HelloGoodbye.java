4958fb92354dd849b2c47f8ce49ac09bc79a599d
ljingr <35159049+ljingr@users.noreply.github.com>
2021-08-17 22:37:20 -0400

    Add files via upload
    Week1 - Programming Assignment #1

---
+++ hello/HelloGoodbye.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Jingran Li
+ *  Coursera User ID:  ljingr@vt.edu
+ *  Last modified:     Aug 15, 2021
+ **************************************************************************** */
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
+    }
+}
---
+++ hello/HelloWorld.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Jingran Li
+ *  Coursera User ID:  ljingr@vt.edu
+ *  Last modified:     Aug 15, 2021
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+    }
+}
